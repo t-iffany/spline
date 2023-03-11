34f05fd3bba8f25cc19d3a3e6af713b3a3532ba6
t-iffany <tiffanyxleong@gmail.com>
2023-03-10 21:51:13 -0800

    Comment out react-app code and import spline component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Spline from '@splinetool/react-spline';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{position: 'relative', width: '100vw', height: '100vh'}}>
+      <Spline scene="https://prod.spline.design/q-hWNcyvaQ3pqUhF/scene.splinecode" />
     </div>
   );
 }
-
-export default App;
